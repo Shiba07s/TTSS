029c4ad9784a3620d0cf7c559d70bb914eaee465
Shiba07s <ashutoshsamantaray166@gmail.com>
2024-01-19 10:26:37 +0530

    integration

---
+++ src/main/java/com/TTSS03/Service/TrainingModeService.java
@@ -0,0 +1,15 @@
+package com.TTSS03.Service;
+
+import java.util.List;
+
+import com.TTSS03.Entity.TrainingMode;
+
+public interface TrainingModeService {
+	List<TrainingMode> getAllTrainingMode();
+
+	TrainingMode saveOneTrainingMode(TrainingMode trainingMode);
+
+	void updateTrainingMode(long trainingModecode, String trainingMode);
+
+	void deActivateTrainingMode(long trainingModecode);
+}
